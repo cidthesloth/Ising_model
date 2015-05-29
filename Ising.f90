2a379d224869baa244748286411fe7b0e654ee32
Justin Dion <justindion90@gmail.com>
2015-05-28 21:26:50 -0700

    Update Ising.f90

--- Ising.f90
+++ Ising.f90
@@ -3,52 +3,67 @@ program MAIN
 implicit none
 	integer, allocatable, DIMENSION(:,:) :: arr
 	real :: num,Temp,timestart,timeend,time,Eavg,Mavg
-	integer*4 N, i,j,t,E,E0,M
+	integer*4 N, i,j,t,E,E0,M,E1,E2
 	Eavg = 0
+	t= 100
 	open (unit =11, file = "IsingEavg.txt") 
 	open (unit =12, file = "IsingMavg.txt")
-
-	call cpu_time(timestart)
-
-	write(*,*) 'Enter the size of matrix: '
-	read(*,*) N
-	write(*,*) 'Enter the amount of iterations: '
-	read(*,*) t
-	ALLOCATE(arr(N,N))
+	open (unit =13, file = "timef90.txt")
 
 
-	do j = 1, N
-		do i = 1,N
-			CALL RANDOM_NUMBER(num)
-			if(num<0.5) Then
-				arr(i,j) = 1
-			Else
-				arr(i,j) = -1
-			END IF
+
+	do N = 10, 52, 2
+		ALLOCATE(arr(N,N))
+		call cpu_time(timestart)
+		do j = 1, N
+			do i = 1,N
+				CALL RANDOM_NUMBER(num)
+				if(num<0.5) Then
+					arr(i,j) = 1
+				Else
+					arr(i,j) = -1
+				END IF
+			end do
 		end do
-	end do
-	print *, arr
-	do j = 1, 51, 1
-		temp = j/10.
-		Eavg= 0
-		Mavg = 0
-		do i = 1, t
-			call flip(arr,E0,E,N,temp)
-			call calcE(N,arr,E,M)
-			Eavg = E+Eavg
-			Mavg = M+Mavg
+		do j = 1, 51, 1
+			temp = j/10.
+			Eavg= 0
+			Mavg = 0
+			!WARM UP
+			call flip(arr,E0,E1,N,temp)
+			call calcE(N,arr,E1,M)
+			call flip(arr,E0,E2,N,temp)
+			call calcE(N,arr,E2,M)
+			do while (E1-E2>N)
+				!Choose a an Energy 3 iterations down and checks its 					difference against initial energy	
+				E1=E2 
+				call flip(arr,E0,E2,N,temp)
+				call calcE(N,arr,E2,M)
+				call flip(arr,E0,E2,N,temp)
+				call calcE(N,arr,E2,M)
+				call flip(arr,E0,E2,N,temp)
+				call calcE(N,arr,E2,M)
+			enddo
+			!End Warm Up
+			do i = 1, t
+				call flip(arr,E0,E,N,temp)
+				call calcE(N,arr,E,M)
+				Eavg = E+Eavg
+				Mavg = M+Mavg
 
+			end do
+			Eavg = Eavg/(N*N*t)
+			Mavg = Mavg/(N*N*t)
+			write(11,*), Eavg
+			write(12,*), Mavg
 		end do
-		Eavg = Eavg/(N*N*t)
-		Mavg = Mavg/(N*N*t)
-		write(11,*), Eavg
-		write(12,*), Mavg
+		call cpu_time(timeend)
+       	 	time = timeend-timestart
+		write(13,*), time
+		DEALLOCATE(arr)
 	end do
-	call cpu_time(timeend)
-        time = timeend-timestart
 	print *, time
 
-	DEALLOCATE(arr)
 end program Main
 
 !***********************************************************************
@@ -105,29 +120,20 @@ end program Main
 
 	subroutine flip(arr,E0,E,N,temp)
 	integer, INTENT(INOUT), DIMENSION(N,N) :: arr
-	integer, DIMENSION(N,N) :: arrnew
 	real :: p,num, temp 
 	integer :: i, j, Enew,E,E0
 	do 50 j = 1, N
          do 60 i = 1, N
-		arrnew = arr
 		call localE(i,j,N,arr,Enew)
 		E0 = Enew
-		if(arr(i,j) == 1) Then
-			arrnew(i,j) = -1
-			call localE(i,j,N,arrnew,Enew)
-		Else
-			arrnew(i,j) = 1
-			call localE(i,j,N,arrnew,Enew)
-		END IF
-
+		Enew = Enew*(-1)
 		if(Enew<=E0) Then
-			arr = arrnew
+			arr(i,j) = (-1)*arr(i,j)
 		else
 			p = exp(-(Enew-E0)/temp)
 			CALL RANDOM_NUMBER(num)
 			if(p>=num) Then
-				arr = arrnew
+				arr(i,j) = (-1)*arr(i,j)
 			END IF
 		END IF
 
