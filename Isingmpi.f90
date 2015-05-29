489695466e8de20835aaaa2f0b3029e70df38baa
Justin Dion <justindion90@gmail.com>
2015-05-28 21:24:45 -0700

    Update Isingmpi.f90

--- Isingmpi.f90
+++ Isingmpi.f90
@@ -1,4 +1,9 @@
 !Ising Model
+!To compile fortran code with MPI
+!mpif90 Isingmpi.f90 -o Isingmpi
+!Run the MPI code with 4 proccessors
+!mpirun -np 4 ./Isingmpi
+
 program MAIN
 
   	! Include the MPI library definitons:
@@ -12,26 +17,10 @@ program MAIN
 	integer*4 N, k,j,t,E,E0,M,x
 	Eavg = 0
 	Mavg = 0
-	open (unit =11, file = "IsingEavg1.txt")
-	open (unit =12, file = "IsingEavg2.txt") 
-	open (unit =13, file = "IsingEavg3.txt") 
-	open (unit =14, file = "IsingEavg4.txt") 
-	open (unit =15, file = "IsingMavg1.txt")
-	open (unit =16, file = "IsingMavg2.txt")
-	open (unit =17, file = "IsingMavg3.txt")
-	open (unit =18, file = "IsingMavg4.txt")
+	t= 100
 
-	call cpu_time(timestart)
 
-	!write(*,*) 'Enter the size of matrix: '
-	!read(*,*) N
-	N = 20
-	t = 1000
-	!write(*,*) 'Enter the amount of iterations: '
-	!read(*,*) t
-	ALLOCATE(arr(N,N))
-
-  ! Initialize the MPI library:
+	  ! Initialize the MPI library:
   call MPI_INIT(ierr)
   if (ierr .ne. MPI_SUCCESS) then
      print *,'Error starting MPI program. Terminating.'
@@ -51,53 +40,73 @@ program MAIN
      print *,'Error getting processor name. Terminating.'
      call MPI_ABORT(MPI_COMM_WORLD, rc, ierr)
   end if
+	open (unit =11, file = "IsingEavg1.txt")
+	open (unit =12, file = "IsingEavg2.txt") 
+	open (unit =13, file = "IsingEavg3.txt") 
+	open (unit =14, file = "IsingEavg4.txt") 
+	open (unit =15, file = "IsingMavg1.txt")
+	open (unit =16, file = "IsingMavg2.txt")
+	open (unit =17, file = "IsingMavg3.txt")
+	open (unit =18, file = "IsingMavg4.txt")
+	open (unit =19, file = "timef90mpi.txt")
 
-	if(rank==0)then
-		do j = 1, N
-			do k = 1,N
-				CALL RANDOM_NUMBER(num)
-				if(num<0.5) Then
-					arr(k,j) = 1
-				Else
-					arr(k,j) = -1
-				END IF
+	call cpu_time(timestart)
+
+	!call MPI_Barrier(MPI_COMM_WORLD)
+	do N = 10, 52, 2
+		call cpu_time(timestart)
+		ALLOCATE(arr(N,N))
+
+		if(rank==0)then
+			do j = 1, N
+				do k = 1,N
+					CALL RANDOM_NUMBER(num)
+					if(num<0.5) Then
+						arr(k,j) = 1
+					Else
+						arr(k,j) = -1
+					END IF
+				end do
 			end do
+
+		end if
+
+
+
+		temp = rank/10.
+
+		do while(temp<5.0)
+			x = int(temp*10)
+			Eavg(x)= 0
+			Mavg(x) = 0
+			do k = 1, t
+				call flip(arr,E0,E,N,temp)
+				call calcE(N,arr,E,M)
+				Eavg(x) = E+Eavg(x)
+				Mavg(x) = M+Mavg(x)
+
+			end do
+			Eavg(x) = Eavg(x)/(N*N*t)
+			Mavg(x) = Mavg(x)/(N*N*t)
+			temp = temp + numtasks/10.
+
 		end do
-	print *, arr
-	end if
 
 
-
-	temp = rank/10.
-
-	do while(temp<5.0)
-		x = int(temp*10)
-		Eavg(x)= 0
-		Mavg(x) = 0
-		do k = 1, t
-			call flip(arr,E0,E,N,temp)
-			call calcE(N,arr,E,M)
-			Eavg(x) = E+Eavg(x)
-			Mavg(x) = M+Mavg(x)
-
-		end do
-		Eavg(x) = Eavg(x)/(N*N*t)
-		Mavg(x) = Mavg(x)/(N*N*t)
-		temp = temp + numtasks/10.
+	
+		write(11+rank,*), Eavg
+		write(15+rank,*), Mavg
+		
+		call cpu_time(timeend)
+        	time = timeend-timestart
+		if(rank==2)then
+			write(19,*), time
+		end if
+		DEALLOCATE(arr)
 
 	end do
-
-	 ! Tell the MPI library to release all resources it is using:
+	! Tell the MPI library to release all resources it is using:
   	call MPI_FINALIZE(ierr)
-	
-	write(11+rank,*), Eavg
-	write(15+rank,*), Mavg
-
-	call cpu_time(timeend)
-        time = timeend-timestart
-	print *, time
-
-	DEALLOCATE(arr)
 end program Main
 
 !***********************************************************************
@@ -159,24 +168,16 @@ end program Main
 	integer :: i, j, Enew,E,E0
 	do 50 j = 1, N
          do 60 i = 1, N
-		arrnew = arr
 		call localE(i,j,N,arr,Enew)
 		E0 = Enew
-		if(arr(i,j) == 1) Then
-			arrnew(i,j) = -1
-			call localE(i,j,N,arrnew,Enew)
-		Else
-			arrnew(i,j) = 1
-			call localE(i,j,N,arrnew,Enew)
-		END IF
-
+		Enew = Enew*(-1)
 		if(Enew<=E0) Then
-			arr = arrnew
+			arr(i,j) = (-1)*arr(i,j)
 		else
 			p = exp(-(Enew-E0)/temp)
 			CALL RANDOM_NUMBER(num)
 			if(p>=num) Then
-				arr = arrnew
+				arr(i,j) = (-1)*arr(i,j)
 			END IF
 		END IF
 
